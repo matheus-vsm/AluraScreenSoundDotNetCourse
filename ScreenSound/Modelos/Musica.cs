3a79f3492adf0ff02f0d4da92a8206d32af1cd58
matheus-vsm <scompinha@hotmail.com>
2025-05-30 22:25:15 -0300

    Add project files.

---
+++ ScreenSound/Modelos/Musica.cs
@@ -0,0 +1,23 @@
+﻿namespace ScreenSound.Modelos;
+
+internal class Musica
+{
+    public string Nome { get; set; }
+    public int Id { get; set; }
+
+    public Musica(string nome)
+    {
+        Nome = nome;
+    }
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {Nome}");
+    }
+
+    public override string ToString()
+    {
+        return @$"Id: {Id}
+        Nome: {Nome}";
+    }
+}
