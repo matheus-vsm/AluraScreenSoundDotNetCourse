a808b3a779fe348c875ae46db5a57577672a3a02
matheus-vsm <scompinha@hotmail.com>
2025-06-11 23:03:32 -0300

    aula 3.9

--- ScreenSound.API/Endpoints/MusicasExtensions.cs
+++ ScreenSound.API/Endpoints/MusicasExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ScreenSound.API.Requests;
 using ScreenSound.Banco;
 using ScreenSound.Modelos;
 
@@ -26,8 +27,12 @@ namespace ScreenSound.API.Endpoints
             });
 
             //Cadastrar Musica
-            app.MapPost("/Musicas", ([FromServices] DAL<Musica> dal, [FromBody] Musica musica) =>
+            app.MapPost("/Musicas", ([FromServices] DAL<Musica> dal, [FromBody] MusicaRequest musicaRequest) =>
             {
+                var musica = new Musica(musicaRequest.nome)
+                {
+                    AnoLancamento = musicaRequest.anoLancamento
+                };
                 dal.Adicionar(musica);
                 return Results.Ok();
             });
