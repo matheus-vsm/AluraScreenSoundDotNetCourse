0c075b0ca4a2a5df2aa613b0142f226d3b0f520e
matheus-vsm <scompinha@hotmail.com>
2025-06-15 22:16:29 -0300

    aula 4.2

--- ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
+++ ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using ScreenSound.Modelos;
+using ScreenSound.Shared.Modelos.Modelos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,7 @@ namespace ScreenSound.Banco
     {
         public DbSet<Artista> Artistas { get; set; }
         public DbSet<Musica> Musicas { get; set; }
+        public DbSet<Genero> Generos { get; set; }
 
         private string conctionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSoundV0;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
 
