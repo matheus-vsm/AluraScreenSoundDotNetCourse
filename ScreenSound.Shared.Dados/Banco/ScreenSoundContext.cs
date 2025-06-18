0391521133aa5923696674238219b29642a17e01
matheus-vsm <scompinha@hotmail.com>
2025-06-18 20:10:07 -0300

    aula 5.3

--- ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
+++ ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
@@ -16,12 +16,14 @@ namespace ScreenSound.Banco
         public DbSet<Musica> Musicas { get; set; }
         public DbSet<Genero> Generos { get; set; }
 
-        private string conctionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSoundV0;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        //conexão local
+        //private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSoundV0;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        private string connectionString = "Server=tcp:screensoundserveralura.database.windows.net,1433;Initial Catalog=ScreenSoundV0;Persist Security Info=False;User ID=matheusadmin;Password=Senha@001;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder
-                .UseSqlServer(conctionString)
+                .UseSqlServer(connectionString)
                 .UseLazyLoadingProxies();
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
