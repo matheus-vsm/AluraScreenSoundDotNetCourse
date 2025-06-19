966ca9537a604353a2267c46785cd2e0350fc008
matheus-vsm <scompinha@hotmail.com>
2025-06-18 22:25:33 -0300

    continuar2

--- ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
+++ ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
@@ -16,7 +16,7 @@ namespace ScreenSound.Banco
         public DbSet<Musica> Musicas { get; set; }
         public DbSet<Genero> Generos { get; set; }
 
-        //conexão local
+        //conexão local 
         private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSoundV0;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
         //conexão Azure
         //private string connectionString = "Server=tcp:screensoundserveralura.database.windows.net,1433;Initial Catalog=ScreenSoundV0;Persist Security Info=False;User ID=matheusadmin;Password=Senha@001;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
