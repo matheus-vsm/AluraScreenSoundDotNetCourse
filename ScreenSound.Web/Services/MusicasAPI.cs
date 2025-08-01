68cea66045d6595fa83f1167c5184114b2f84ac5
matheus-vsm <scompinha@hotmail.com>
2025-07-31 21:51:24 -0300

    aula 4.5

--- ScreenSound.Web/Services/MusicasAPI.cs
+++ ScreenSound.Web/Services/MusicasAPI.cs
@@ -1,4 +1,5 @@
-﻿using ScreenSound.Web.Response;
+﻿using ScreenSound.Web.Requests;
+using ScreenSound.Web.Response;
 using System.Net.Http.Json;
 
 namespace ScreenSound.Web.Services
@@ -13,10 +14,33 @@ namespace ScreenSound.Web.Services
 
         //Método assíncrono (async) que retorna uma lista de musicas (MusicasResponse).
         //Faz uma requisição GET para o endpoint musicas da API.
-        public async Task<ICollection<MusicaResponse>?> GetArtistasAsync()
+        public async Task<ICollection<MusicaResponse>?> GetMusicasAsync()
         {
             //O método GetFromJsonAsync<T>() já faz o GET e converte a resposta JSON para objetos C#.
             return await _httpClient.GetFromJsonAsync<ICollection<MusicaResponse>>("musicas");
+        }
+
+        public async Task AddMusicaAsync(MusicaRequest musica)
+        {
+            await _httpClient.PostAsJsonAsync("musicas", musica);
+        }
+
+        public async Task DeleteMusicaAsync(int id)
+        {
+            //Deleta um Musica específico pelo ID.
+            await _httpClient.DeleteAsync($"musicas/{id}");
+        }
+
+        public async Task AtualizarMusicaAsync(MusicaRequestEdit musica)
+        {
+            //Atualiza um Musica específico pelo ID.
+            await _httpClient.PutAsJsonAsync($"artistas", musica);
+        }
+
+        public async Task<MusicaResponse?> GetMusicaPorNomeAsync(string nome)
+        {
+            //Busca um Musica específico pelo ID.
+            return await _httpClient.GetFromJsonAsync<MusicaResponse>($"musicas/{nome}");
         }
     }
 }
